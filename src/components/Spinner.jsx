41cff9843988ab36268499e67959974d840dfbe7
slugneffex <rawatmayank969@gmail.com>
2023-06-26 18:24:45 +0530

    shopping-cart updated the whole website

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const spinner = () => {
+    return (
+        <div>
+            <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default spinner
